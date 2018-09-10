e4798ceb44eea7f9c8d2b363724b9c4deca1135f
Shane EC Davis <secd0183@yahoo.com>
2018-09-10 18:01:13 -0500

    created db cart model

---
+++ server/db/models/cart.js
@@ -0,0 +1,12 @@
+const crypto = require('crypto')
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Cart = db.define('cart', {
+  product: {
+    type: Sequelize.JSON
+  }
+})
+
+
+module.exports = Cart
