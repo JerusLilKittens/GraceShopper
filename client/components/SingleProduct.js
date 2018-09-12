bbb659e9926f187aa2ea1a8552631752de51dd45
Stasha Yancho <stasha.yancho@gmail.com>
2018-09-11 21:05:04 -0500

    adding single product view and test

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Rating, Icon, Image, Item } from 'semantic-ui-react'
+
+const SingleProduct = (props) => {
+  const product = props.product
+  return (
+    <div id="single-product">
+      <Item>
+        <Item.Image size='small' src={product.imageUrl} />
+
+        <Item.Content>
+          <Item.Header as='a'>{product.name}</Item.Header>
+          <Rating icon='star' defaultRating={0} maxRating={5} />
+          <Item.Description>{product.description}</Item.Description>
+          <Item.Extra>
+            <Icon color='yellow' name='dollar sign' /> {product.price}
+          </Item.Extra>
+        </Item.Content>
+      </Item>
+    </div>
+  )
+}
+
+
+const mapStateToProps = state => {
+  return {
+    product: state.selectedProduct
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
