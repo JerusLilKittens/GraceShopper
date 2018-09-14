55f4d2193855703d20cd84b9238738f686bdf9f9
Stasha Yancho <stasha.yancho@gmail.com>
2018-09-14 09:42:50 -0500

    working on posting reviews

--- client/store/index.js
+++ client/store/index.js
@@ -4,11 +4,13 @@ import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
 import {productsReducer, productReducer} from './product'
+import { reviewReducer } from './review';
 
 const reducer = combineReducers({
   user,
   products: productsReducer,
-  selectedProduct: productReducer
+  selectedProduct: productReducer,
+  review: reviewReducer
 })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
